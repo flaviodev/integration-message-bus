97b8d7b63d79c7b36f6c57540a3f7c050d19cad3
flavio <fdsdev@gmail.com>
2018-10-03 23:44:40 -0300

    removing bdd mocks

--- src/test/java/com/github/flaviodev/messagebus/MessageBusTest.java
+++ src/test/java/com/github/flaviodev/messagebus/MessageBusTest.java
@@ -4,7 +4,6 @@ import static org.awaitility.Awaitility.await;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.BDDMockito.given;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,9 +16,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.github.flaviodev.imb.IntegrationMessageBusApplication;
-import com.github.flaviodev.imb.messagebus.ConsumeEmployeeUpdateRedirectConfig;
-import com.github.flaviodev.imb.messagebus.ConsumeEmployeeUpdateTenant1Config;
+import com.github.flaviodev.imb.messagebus.ConsumeEmployeeUpdateTenant2Config;
 import com.github.flaviodev.imb.messagebus.base.MessageBusAdmin;
+import com.github.flaviodev.imb.messagebus.base.MessageTopic;
 import com.github.flaviodev.imb.model.Employee;
 import com.github.flaviodev.imb.persistence.UUIDGenerator;
 import com.github.flaviodev.imb.repository.EmployeeRepository;
@@ -31,18 +30,14 @@ import com.google.common.collect.ImmutableMap;
 @ContextConfiguration(classes = IntegrationMessageBusApplication.class)
 public class MessageBusTest {
 
-	private static final String TOPIC_TEST = "employee-test";
-	private static final String SUBSCRIPTION_TEST = "subscription-employee-test";
-
 	private static final String GROUP_TENANT1_TEST = "26587a2c89be46b895d6d0f14d182d1a";
+
+	private static final ImmutableMap<String, String> HEADER = ImmutableMap.of("routingKey", GROUP_TENANT1_TEST);
 
 	private static final String NAME_EMPLOYEE_TEST = "Employee_" + UUIDGenerator.uuid();
 
 	@MockBean
-	private ConsumeEmployeeUpdateRedirectConfig consumeEmployeeUpdateRedirect;
-
-	@MockBean
-	private ConsumeEmployeeUpdateTenant1Config consumeEmployeeUpdateTenant1;
+	private ConsumeEmployeeUpdateTenant2Config consumeEmployeeUpdateTenant2;
 
 	@Autowired
 	private MessageBusAdmin messageBusAdmin;
@@ -53,69 +48,22 @@ public class MessageBusTest {
 	@Test
 	public void shouldSendAndReceiveMessage() {
 
-		mockingConsumeEmployeeUpdateRedirectMethods();
-		mockingConsumeEmployeeUpdateTenant1Methods();
-
-		creatingSubscriptions();
-
 		simulatePublicationOnTopic();
 
-		consumeEmployeeUpdateRedirect.consumeMessage();
-
-		consumeEmployeeUpdateTenant1.consumeMessage();
-
 		waitingConsumeEmployeeUpdateTenant1();
-
-		deletingTopicsAndSubscriptionsCreatedsForTest();
 
 		assertEquals(NAME_EMPLOYEE_TEST, employeeRepository.findByName(NAME_EMPLOYEE_TEST).getName());
 	}
 
-	private void mockingConsumeEmployeeUpdateRedirectMethods() {
-		given(consumeEmployeeUpdateRedirect.getMessageBusAdmin()).willReturn(messageBusAdmin);
-		given(consumeEmployeeUpdateRedirect.getTopicName()).willReturn(TOPIC_TEST);
-		given(consumeEmployeeUpdateRedirect.getSubscriptionName()).willReturn(SUBSCRIPTION_TEST);
-		given(consumeEmployeeUpdateRedirect.getGroupName()).willReturn("");
-		given(consumeEmployeeUpdateRedirect.getRedirectGroupName(ImmutableMap.of("routingKey", GROUP_TENANT1_TEST)))
-				.willReturn(GROUP_TENANT1_TEST);
-		given(consumeEmployeeUpdateRedirect
-				.doesMeetTheRedirectionCondition(ImmutableMap.of("routingKey", GROUP_TENANT1_TEST))).willReturn(true);
-		given(consumeEmployeeUpdateRedirect.consumeMessage()).willCallRealMethod();
-	}
-
-	private void mockingConsumeEmployeeUpdateTenant1Methods() {
-		given(consumeEmployeeUpdateTenant1.getMessageBusAdmin()).willReturn(messageBusAdmin);
-		given(consumeEmployeeUpdateTenant1.getTopicName()).willReturn(TOPIC_TEST);
-		given(consumeEmployeeUpdateTenant1.getSubscriptionName()).willReturn(SUBSCRIPTION_TEST);
-		given(consumeEmployeeUpdateTenant1.getGroupName()).willReturn(GROUP_TENANT1_TEST);
-		given(consumeEmployeeUpdateTenant1.consumeMessage()).willCallRealMethod();
-	}
-
-	private void creatingSubscriptions() {
-		messageBusAdmin.createSubscriptionForTopic(SUBSCRIPTION_TEST, TOPIC_TEST);
-		await().until(() -> messageBusAdmin.isRegistredSubscription(SUBSCRIPTION_TEST));
-
-		messageBusAdmin.createSubscriptionForTopic(SUBSCRIPTION_TEST, TOPIC_TEST, GROUP_TENANT1_TEST);
-		await().until(() -> messageBusAdmin.isRegistredSubscription(SUBSCRIPTION_TEST, GROUP_TENANT1_TEST));
-	}
-
 	private void simulatePublicationOnTopic() {
-		messageBusAdmin.publishMessage(TOPIC_TEST, Employee.class, new Employee(NAME_EMPLOYEE_TEST),
-				ImmutableMap.of("routingKey", GROUP_TENANT1_TEST));
+		messageBusAdmin.publishMessage(MessageTopic.UPDATE_EMPLOYEE.getName(), Employee.class,
+				new Employee(NAME_EMPLOYEE_TEST), HEADER);
 	}
 
 	private void waitingConsumeEmployeeUpdateTenant1() {
 		TenantContext.setCurrentTenant(GROUP_TENANT1_TEST);
 		await().pollDelay(5, TimeUnit.SECONDS).timeout(60, TimeUnit.SECONDS).ignoreExceptions()
 				.until(() -> employeeRepository.findByName(NAME_EMPLOYEE_TEST) != null);
-	}
-
-	private void deletingTopicsAndSubscriptionsCreatedsForTest() {
-		messageBusAdmin.deleteSubscription(SUBSCRIPTION_TEST);
-		messageBusAdmin.deleteTopic(TOPIC_TEST);
-
-		messageBusAdmin.deleteSubscription(SUBSCRIPTION_TEST, GROUP_TENANT1_TEST);
-		messageBusAdmin.deleteTopic(TOPIC_TEST, GROUP_TENANT1_TEST);
 	}
 
 	@Test
@@ -153,5 +101,4 @@ public class MessageBusTest {
 
 		messageBusAdmin.deleteTopic("employee2");
 	}
-
 }
